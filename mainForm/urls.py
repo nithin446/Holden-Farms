21f3f4b74196d5562a14c6a7fc5cd18aa1620d6d
Jordan Roth <jordan97@ksu.edu>
2017-04-26 21:41:42 -0500

    Deleted .pyc files, fixed the login url, and added ‘localhost’ to allowed hosts

--- mainForm/urls.py
+++ mainForm/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns=[
     url(r'^$', views.pig_form, name='index'),
     url(r'^submissions/$', views.farm_submissions, name='farm_submissions'),
-    url(r'^login/$', auth_views.login, {'template_name': 'mainForm\login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout,{'next_page': '/login/'}, name='logout'),
+    url(r'^login/$', auth_views.login, {'template_name': 'mainForm/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/login/'}, name='logout'),
 ]
